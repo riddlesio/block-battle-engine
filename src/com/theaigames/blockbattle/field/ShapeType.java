17b3f7a3dd318bc97ddfc83b318def5d79186bc9
Jim van Eeden <jim_v_eeden@hotmail.com>
2015-07-15 10:52:32 +0200

    using secure random generator now

--- src/com/theaigames/blockbattle/field/ShapeType.java
+++ src/com/theaigames/blockbattle/field/ShapeType.java
@@ -1,12 +1,12 @@
 package com.theaigames.blockbattle.field;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public enum ShapeType {
 	I, J, L, O, S, T, Z, NONE;
 	
 	private static final ShapeType[] VALUES = ShapeType.values();
-	private static final Random RANDOM = new Random();
+	private static final SecureRandom RANDOM = new SecureRandom();
 	private static final int SIZE_SHAPES = VALUES.length - 1;
 	
 	/**
